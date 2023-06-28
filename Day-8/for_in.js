d48f87fbb4a10431eb9e2120ef253eeaca44ceac
Tirth khunt <137253787+Tirthkhunt@users.noreply.github.com>
2023-06-28 12:48:08 +0530

    Delete Day-8 directory

--- Day-8/for_in.js
+++
@@ -1,8 +0,0 @@
-
-const numbers = [45, 4, 9, 16, 25];
-
-let txt = "";
-for (let x in numbers) {
-  txt += numbers[x] + "<br>"; 
-}
-document.getElementById("demo").innerHTML = txt;
--- Day-8/write_log.js
+++
@@ -1,3 +0,0 @@
-var a=10;
-        document.write("Value of a is :",a);
-        console.log("Value of a is :",a);
